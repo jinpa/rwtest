7474a72202ce1bd88706507278e73b19beb564ac
Jane Manning <jmanning@gmail.com>
2025-12-24 11:34:44 -0800

    fix ref to routes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(routes());
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
